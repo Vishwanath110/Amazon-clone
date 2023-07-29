51068f60f1408f915fe2bdf16c75132dd8301a86
Vishwanath k <vishwagowda001@gmail.com>
2023-07-29 22:16:31 +0530

    Footer file added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import Header from './Header';
 import {  Route, Routes } from 'react-router-dom';
 import Home from './Home';
+import Last from './Last';
 
 
 function App() {
   return (
       <div className="app">
       <Routes>
-      <Route  path="/" element={<><Header/><Home/></>}/>
+      <Route  path="/" element={<><Header/><Home/><Last/></>}/>
 
-      <Route exact path="/cservice" element={<><Header/></>}/>
+      <Route exact path="/cservice" element={<><Header/><Last/></>}/>
       </Routes>
       </div>
     
---
+++ src/Last.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Last.css"
+
+function Last() {
+  return (
+    <div>
+        <div className='backtotop'>
+        Back to top
+        </div>
+
+        <div className="lfirst">
+            <div>
+                Get to Know Us <br /> Careers <br /> Blog <br /> About Amazon <br /> Investor Relations <br />Amazon Devices <br />Amazon Science
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Last
